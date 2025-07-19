61419ec401c82414a29d7ced2773c7ff37c7938b
Algent47 <pritiwalunj21@gmail.com>
2025-07-19 22:19:44 +0530

    Add .next/static/chunks/src_app_travel_page_tsx_a993527f._.js 1752943783535

---
+++ .next/static/chunks/src_app_travel_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_208329fa._.js",
+  "static/chunks/src_ac918b52._.js"
+],
+    source: "dynamic"
+});
